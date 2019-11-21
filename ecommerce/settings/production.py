9e45d16615929cf3a5fdfca8f91bfec1492129ed
dannv6 <dannv6@fpt.com.vn>
2019-11-21 17:20:04 +0700

    use python decouple

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -2,20 +2,20 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
     }
 }
 
-STRIPE_PUBLIC_KEY = ''
-STRIPE_PRIVATE_KEY = ''
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_PRIVATE_KEY = config('STRIPE_PRIVATE_KEY')
