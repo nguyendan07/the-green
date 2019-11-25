6858390000ab6de86abde87da5f80518366a2382
dannv6 <dannv6@fpt.com.vn>
2019-11-25 15:18:50 +0700

    Add template files and static files

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Item
+
+
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+
+    return render(request, 'index.html', context)
