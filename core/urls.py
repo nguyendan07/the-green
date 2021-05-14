dac607164d68c6a7fc83039042e05fafe0adccf3
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2021-05-14 10:45:01 +0700

    Creating an order summary

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('shop', views.ItemListView.as_view(), name='shop'),
     path('contact', views.contact, name='contact'),
     path('product/<slug>/', views.ItemDetailView.as_view(), name='product-detail'),
+    path('checkout/cart', views.OrderSummaryView.as_view(), name='checkout-cart'),
     path('add-to-cart/<slug>', views.add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>', views.remove_from_cart, name='remove-from-cart'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views.generic import DetailView, ListView
@@ -9,6 +11,9 @@ class HomeView(ListView):
     model = Item
     template_name = 'index.html'
 
+    # def get_queryset(self) -> QuerySet:
+    #     return super().get_queryset()
+
 
 class ItemListView(ListView):
     model = Item
@@ -17,11 +22,24 @@ class ItemListView(ListView):
     template_name = 'shop.html'
 
 
+class OrderSummaryView(LoginRequiredMixin, ListView):
+    model = Order
+    template_name = 'cart.html'
+    context_object_name = 'order_list'
+
+    def get_queryset(self):
+        try:
+            return Order.objects.get(user=self.request.user, ordered=False)
+        except Order.DoesNotExist:
+            return None
+
+
 class ItemDetailView(DetailView):
     model = Item
     template_name = 'product-single.html'
 
 
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(item=item,
@@ -42,6 +60,7 @@ def add_to_cart(request, slug):
     return redirect("core:product-detail", slug=slug)
 
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
